adc43fbd8e2157a1a6f93819e571d73d98e8ae86
Filip Nedovic <filip_nedovic@epam.com>
2022-12-15 17:21:46 +0100

    included login via backend custom command for later tests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,18 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("loginBE", () => {
+  cy.request({
+    method: "POST",
+    url: "https://cypress-api.vivifyscrum-stage.com/api/v2/login ",
+    body: {
+      email: Cypress.env("testUserEmail"),
+      password: Cypress.env("testUserPass"),
+    },
+  }).then((response) => {
+    window.localStorage.setItem("token", response.body.token);
+    window.localStorage.setItem("user", JSON.stringify(response.body.user));
+    window.localStorage.setItem("user_id", response.body.user.id);
+  });
+});
